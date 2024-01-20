7b6e9a20fcf63010e8216a58c309a806a4f441f7
VisvShah <visvshah1@gmail.com>
2024-01-20 14:47:05 -0500

    signin/signup works

--- pulse/src/pages/signup.tsx
+++ pulse/src/pages/signup.tsx
@@ -1,18 +1,36 @@
 import React, {useState} from 'react'
-
-
-export default function auth() {
+import { auth } from '~/utils/firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+export default function Signup() {
     const [email, setEmail] = useState("");
-const [password, setPassword ] = useState("");
+    const [password, setPassword ] = useState("");
+    const [signUp, changeSignUp] = useState(true)
 const submitForm = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (signUp) {
+        createUserWithEmailAndPassword(auth, email, password).then((userCredentials) => {
+            console.log(userCredentials);
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+    else {
+        signInWithEmailAndPassword(auth, email, password).then((userCredentials) => {
+            console.log(userCredentials);
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
 }
   return (
     <div className="flex justify-center items-center">
     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  Sign up/in to your account
+                  {(signUp) ? "Sign Up" : "Sign In"}
               </h1>
               <form onSubmit={submitForm} className="space-y-4 md:space-y-6" action="#">
                   <div>
@@ -24,8 +42,11 @@ const submitForm = (e: React.FormEvent<HTMLFormElement>) => {
                       <input onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
                   </div>
             
-                  <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                  <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">{(signUp) ? "Sign Up" : "Sign In"}</button>
               </form>
+              <p onClick={()=>changeSignUp(!signUp)} className="text-sm justify-center font-light text-gray-500 dark:text-gray-400">
+                     <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">{(signUp) ? "Sign In Instead" : "Sign Up Instead"}</a>
+            </p>
           </div>
     </div>
     </div>
--- pulse/src/utils/firebase.ts
+++ pulse/src/utils/firebase.ts
@@ -1,9 +1,11 @@
 import firebase from "firebase/app"
 import { initializeApp } from "firebase/app";
 import "firebase/auth"
+import { getAuth } from "firebase/auth";
+import { env } from "~/env.js";
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
+    apiKey:"AIzaSyDmPys9pcGJocmolSPAm2OW2p0jQdL5zu0",
     authDomain: "auth-development-f2bc9.firebaseapp.com",
     projectId: "auth-development-f2bc9",
     storageBucket: "auth-development-f2bc9.appspot.com",
@@ -12,3 +14,4 @@ const firebaseConfig = {
   };
 
 export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
