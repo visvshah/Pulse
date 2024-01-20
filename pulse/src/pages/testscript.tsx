c9ccbe3e5eed939c5b1de16d84e1e029b5076a82
VisvShah <visvshah1@gmail.com>
2024-01-20 04:14:15 -0500

    Merged but can't test

--- pulse/src/pages/testscript.tsx
+++ pulse/src/pages/testscript.tsx
@@ -18,53 +18,56 @@ export default function Home() {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault(); // Prevents the default form submission behavior
-    try {
-      const response = await fetch("/api/createSummary", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ presentation }) ,
-      });
-
-
-      if (response.status === 200) {
-        const result = await response.json();
-        const res = result[0].message.content
-        console.log("RES: " + res);
-        setText(JSON.parse(res) as Summaries);
-        const summaries = JSON.parse(res).topics;
-        console.log(summaries.length);
-        setScript([]);
-        for (let i = 0; i < summaries.length; i++) {
-          const topic_name = summaries[i]?.topic_name as string;
-          const topic_summary = summaries[i]?.topic_summary as string;
-          console.log(topic_name);
-          console.log(topic_summary);
-
-          const response2 = await fetch("/api/createScripts", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ topic_name, topic_summary }) ,
-          });
-          const result2 = await response2.json();
-          console.log(result2)
-          const res2 = result2[0].message.content;
-          console.log(i + ": " + res2)
-          
-          setScript((prevScripts) => [
-            ...prevScripts,
-            { topic_name, topic_script: res2 },
-          ]);
-        }
-      }
-    } catch (error) {
-      console.error("Error:", error);
-    }
+    createScripts();
   };
 
+  const createScripts = async() => {
+    try {
+    const response = await fetch("/api/createSummary", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ presentation }) ,
+    });
+
+
+    if (response.status === 200) {
+      const result = await response.json();
+      const res = result[0].message.content
+      console.log("RES: " + res);
+      setText(JSON.parse(res) as Summaries);
+      const summaries = JSON.parse(res).topics;
+      console.log(summaries.length);
+      setScript([]);
+      for (let i = 0; i < summaries.length; i++) {
+        const topic_name = summaries[i]?.topic_name as string;
+        const topic_summary = summaries[i]?.topic_summary as string;
+        console.log(topic_name);
+        console.log(topic_summary);
+
+        const response2 = await fetch("/api/createScripts", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ topic_name, topic_summary }) ,
+        });
+        const result2 = await response2.json();
+        console.log(result2)
+        const res2 = result2[0].message.content;
+        console.log(i + ": " + res2)
+        
+        setScript((prevScripts) => [
+          ...prevScripts,
+          { topic_name, topic_script: res2 },
+        ]);
+      }
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
   const uploadSlides = async () => {
     if (file) {
       const res = await edgestore.publicFiles.upload({
@@ -97,6 +100,9 @@ export default function Home() {
         if (response.ok) {
           const result = await response.json();
           console.log('API Response:', result.text);
+
+          setpresentation(result.text);
+          handleSubmit();
         } else {
           console.error('API Error:', response.statusText);
         }
@@ -134,7 +140,7 @@ export default function Home() {
       </div>
 
       <div className="flex flex-col bg-slate-500 w-[100vw] h-[50vh] overflow-scroll">
-        <h1>Summaries</h1>
+        <h1 className="m-3">Summaries</h1>
         {text.topics.map((obj) => (
           <div className="flex flex-row">
             <p><b>{obj.topic_name}</b>: {obj.topic_summary}</p>
