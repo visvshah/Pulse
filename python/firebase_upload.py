c285370dad630a1d2ed4a1e0949e6893ce6045d2
sarthak <sarthak.robo@gmail.com>
2024-01-20 23:15:45 -0500

    add srng files

---
+++ python/firebase_upload.py
@@ -0,0 +1,19 @@
+from firebase_admin import credentials, initialize_app, storage
+
+def upload_file(source, dest):
+	# source -> source file on local machine /folder/vid.mp4
+	# dest -> dest location on firebase i.e videos/vid.mp4 
+	# (dont add /videos, it makes a new folder '/' and a folder videos in /)
+	# it makes entire dest folder path in firebase 
+	# name of video is needed in both source and dest
+	cred = credentials.Certificate("./accountKey.json")
+	initialize_app(cred, {'storageBucket': 'auth-development-f2bc9.appspot.com'})
+
+	bucket = storage.bucket()
+	blob = bucket.blob(dest)
+	blob.upload_from_filename(source)
+	blob.make_public()
+	# print("your file url", blob.public_url)
+	return blob.public_url
+
+# upload_file("./brainrot/gta.mp4", "videos/gta.mp4")
--- vid-transcription/test/main.py
+++
@@ -1,6 +0,0 @@
-import modal
-f = modal.Function.lookup("wav2lip-simple", "Wav2LipModel.run_whisper")
-with open("sound.mp3", "rb") as audio_file:
-  input_audio_bytes = audio_file.read()
-g = f.remote(input_audio_bytes)
-print(g)
