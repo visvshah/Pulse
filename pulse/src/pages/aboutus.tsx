773e76c86238d5c82cd55f2c3ce5f8cacd1fb219
Saarang Srinivasan <saarangsrini35@gmail.com>
2024-01-21 02:28:40 -0500

    fixed but still broken aboutme

--- pulse/src/pages/aboutus.tsx
+++ pulse/src/pages/aboutus.tsx
@@ -8,7 +8,6 @@ import React from "react";
 import { useEdgeStore } from '../lib/edgestore';
 
 export default function Page() {
-  //const [url, setUrl] = React.useState<string>();
 
   return (
     <>
@@ -28,14 +27,14 @@ export default function Page() {
         </ul>
       </nav>*/}
 
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0"> </div>
 
       <h1 className="flex items-center z-10 text-4xl hover:scale-110 text-transparent duration-1000 cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text bg-white p-5">
         Pulse
         <Image alt='xyz' width={100} height={100} src=null className="float-right rounded-full mx-4" />
       </h1>
 
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0"> </div>
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-lg text-zinc-500">
           <p>Hi, my name is Saarang!</p>
